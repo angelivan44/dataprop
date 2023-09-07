85208f8caf7df12fdcf771dd95829ba6dde05efc
Angel <angelhuays@gmail.com>
2023-09-06 20:11:52 -0500

    add photo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_07_010313) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_07_011040) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_010313) do
     t.index ["user_id"], name: "index_buildings_on_user_id"
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.integer "priority"
+    t.bigint "building_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["building_id"], name: "index_photos_on_building_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_010313) do
   end
 
   add_foreign_key "buildings", "users"
+  add_foreign_key "photos", "buildings"
 end
