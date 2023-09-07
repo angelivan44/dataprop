d74f3304a61ecac0c1ef53aa5771cf80083dd7b1
Angel <angelhuays@gmail.com>
2023-09-06 21:59:35 -0500

    add building model crud whit views

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,5 +1,48 @@
 class BuildingsController < ApplicationController
+  before_action :set_building, only: [:show, :edit, :update, :destroy]
+
   def index
     @builds = Building.all
+  end
+
+  def new
+    @building = Building.new
+  end
+
+  def create
+    @building = Building.new(building_params)
+    @building.user = User.first
+    if @building.save
+      redirect_to buildings_path(@building)
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @building.update(building_params)
+      redirect_to @building, notice: 'Building was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @building.destroy
+    redirect_to buildings_url, notice: 'Building was successfully destroyed.'
+  end
+
+  private
+
+  def set_building
+    @building = Building.find(params[:id])
+  end
+
+  def building_params
+    params.require(:building).permit(:address, :area_sq_meter, :price, :transaction_type, :bedroom_count, :bathroom_count )
   end
 end
